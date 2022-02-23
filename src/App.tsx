102cc5fafb1c9c1bfb1f5d7de62e8c6b9f8cb986
Jose Henrique <zehenrique0822@gmail.com>
2022-02-23 13:11:46 -0300

    finalizacao area de insercao

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,41 @@ import { items } from './data/items';
 import { categories } from './data/categories';
 import { FilterListByMonth, getCurrentMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(()=> {
 
     setFilteredList( FilterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
+
+  useEffect(()=> {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredList){
+      if(categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+
+  }, [filteredList]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
 
   return(
     <C.Container>
@@ -27,7 +51,12 @@ const App = () => {
         {/* Área de informações */}
 
         {/* Área de inserção */}
-
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
         {/* Tabela de itens */}
         <TableArea list = {filteredList}/>
       </C.Body>
